fa53be17742602516c4699644772b99d1ed4c06e
Ronu Ghoshal <email": null>
2016-04-22 20:16:36 -0700

    Update stops controller to return JSON

--- last-stop/app/controllers/stops_controller.rb
+++ last-stop/app/controllers/stops_controller.rb
@@ -1,6 +1,7 @@
 class StopsController < ApplicationController
   def index
-    @stops = Stop.all
+    #@stops = Stop.near(params[:location])
+    @stops = Stop.near([37.464763, -122.197985])
     render json: @stops
   end
 
