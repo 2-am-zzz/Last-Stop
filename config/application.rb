e9cb9bfae3f0056db273e32b0649bc3ad81f7cd0
tmashuang <tmashuang@gmail.com>
2016-04-24 12:30:06 -0700

    In process

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ Bundler.require(*Rails.groups)
 
 module LastStop
   class Application < Rails::Application
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
