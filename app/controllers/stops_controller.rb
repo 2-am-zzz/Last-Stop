80ea17116240e9113a54de128183326232ca8fc8
Cyrus <email": null>
2016-04-23 19:36:48 -0700

    Add new server test in controller

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -3,7 +3,8 @@ class StopsController < ApplicationController
     #@stops = Stop.near(params[:location])
 
     p "* " * 90
-    @stops = Stop.near([37.464763, -122.197985])
+    # @stops = Stop.near([37.464763, -122.197985])
+    @stops = Stop.test
     p @stops
     p "* " * 90
     render json: @stops
