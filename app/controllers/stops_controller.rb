b4ca9afc6464473043973f1f2a04591b52d76c11
Cyrus <email": null>
2016-04-23 20:00:12 -0700

    Finalize working mongoid config file for deployment

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,12 +1,7 @@
 class StopsController < ApplicationController
   def index
     #@stops = Stop.near(params[:location])
-
-    p "* " * 90
-    # @stops = Stop.near([37.464763, -122.197985])
-    @stops = Stop.test
-    p @stops
-    p "* " * 90
+    @stops = Stop.near([37.464763, -122.197985])
     render json: @stops
   end
 
