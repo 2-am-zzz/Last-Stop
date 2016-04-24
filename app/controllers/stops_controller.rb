b07ae4c08fa37c4145261ebabdca7924382816b4
Cyrus <email": null>
2016-04-24 16:01:39 -0700

    Debugging params.

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,6 +1,9 @@
 class StopsController < ApplicationController
 
   def index
+    p "*"*50
+    p params[:lat]
+    p params[:lon]
     @stops = Stop.near([params[:lat],params[:lon]])
     # @stops = Stop.near([37.464763, -122.197985])
     render json: @stops
