2e56446d1c7ace22647f8ab5f9c5588fa1af2375
Cyrus <email": null>
2016-04-23 19:45:40 -0700

    Test add for variables in config

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 require 'mongoid'
-require 'mongo'
-Mongoid.load!(File.expand_path('mongoid.yml', './config'))
+Mongoid.load!('mongoid.yml', :production)
+# Mongoid.load!(File.expand_path('mongoid.yml', './config'))
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
