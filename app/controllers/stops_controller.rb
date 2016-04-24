22d8c01d15d71adf3b446c4b5628e70dda25bfea
Cyrus <email": null>
2016-04-24 16:02:55 -0700

    Convert params into floats.

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,10 +1,7 @@
 class StopsController < ApplicationController
 
   def index
-    p "*"*50
-    p params[:lat]
-    p params[:lon]
-    @stops = Stop.near([params[:lat],params[:lon]])
+    @stops = Stop.near([params[:lat].to_f,params[:lon].to_f])
     # @stops = Stop.near([37.464763, -122.197985])
     render json: @stops
   end
