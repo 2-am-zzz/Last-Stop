bc377ba9c929ed0778b915fa1c7ea7fd957b8b5d
Cyrus <email": null>
2016-04-23 17:10:50 -0700

    debug

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -2,7 +2,11 @@ class StopsController < ApplicationController
   def index
     #@stops = Stop.near(params[:location])
     # @stops = Stop.near([37.464763, -122.197985])
+
     @stops = Stop.test
+    p "* " * 90
+    p @stops
+    p "* " * 90
     render json: @stops
   end
 
