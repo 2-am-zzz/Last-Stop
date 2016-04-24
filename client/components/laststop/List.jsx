87296e181b6d7dbac2797f5bebde309bfbb9352b
tmashuang <tmashuang@gmail.com>
2016-04-24 13:02:58 -0700

    Starting on view

--- client/components/laststop/List.jsx
+++ client/components/laststop/List.jsx
@@ -1,47 +1,41 @@
 var React = require('react');
-var ReactDOM = require('react-dom');
 var request = require('superagent');
 
   var List = React.createClass({
     getInitialState: function() {
       return {
-        agency: "",
-        departure_time: "",
-        direction_id: "",
-        stop_lat: "",
-        stop_lon: "",
-        stop_name:"",
+        // data: ({
+        //
+        // })
       };
     },
     componentWillMount: function() {
       request
-        .get('http://last-stop.herokuapp.com/apis/stops')
+        .get('https://last-stop.herokuapp.com/apis/stops')
         .end(function(err, res) {
-          var data = JSON.parse(res.text)
-          this.setState({
-            agency: data.agency_id,
-            departure_time: data.departure_time,
-            direction_id: data.direction_id,
-            stop_lat: data.stop_lat,
-            stop_lon: data.stop_lon,
-            stop_name:data.stop_name,
-          });
-      }.bind(this));
+          var data = JSON.parse(res.text);
+      });
     },
-    render: function() {
-      return (
-        <div>
-          {this.state.agency}
-          <ul>
-            <li>{this.state.departure_time}</li>
-            <li>{this.state.direction_id}</li>
-            <li>{this.state.stop_lat}</li>
-            <li>{this.state.stop_lon}</li>
-            <li>{this.state.stop_name}</li>
-          </ul>
-        </div>
-      );
+  });
+
+  var Agency = React.createClass({
+    render: function(){
+      debugger;
     }
   });
+  //  render: function() {
+  //     return (
+  //       <div>
+  //         {this.state.agency}
+  //         <ul>
+  //           <li>{this.state.departure_time}</li>
+  //           <li>{this.state.direction_id}</li>
+  //           <li>{this.state.stop_lat}</li>
+  //           <li>{this.state.stop_lon}</li>
+  //           <li>{this.state.stop_name}</li>
+  //         </ul>
+  //       </div>
+  //     );
+  //   };
 
 module.exports = List;
