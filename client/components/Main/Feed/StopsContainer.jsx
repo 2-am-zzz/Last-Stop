036e88fa251ba5eb1368c9b505d3cf29f1f9fe5e
Ronu Ghoshal <email": null>
2016-04-28 12:04:12 -0700

    Integrate stopsContainer logic.

--- client/components/Main/Feed/StopsContainer.jsx
+++ client/components/Main/Feed/StopsContainer.jsx
@@ -8,8 +8,31 @@ var StopsContainer = React.createClass({
   getInitialState: function() {
     return {
       stops: null,
+      allStops: null,
       agencies: null
     };
+  },
+
+  updateStops: function(agency) {
+    if (agency == 0) {
+      this.setState({stops: this.state.allStops});
+    } else if (agency == 1) {
+      this.setState({stops: this.state.allStops.filter(function(stop) {
+        return stop.agency_id == "AC TRANSIT"
+      })})
+    } else if (agency == 2) {
+      this.setState({stops: this.state.allStops.filter(function(stop) {
+        return stop.agency_id == "BART"
+      })})
+    } else if (agency == 3) {
+      this.setState({stops: this.state.allStops.filter(function(stop) {
+        return stop.agency_id == "SFMTA"
+      })})
+    } else if (agency == 4) {
+      this.setState({stops: this.state.allStops.filter(function(stop) {
+        return stop.agency_id == "CT"
+      })})
+    }
   },
 
   getInfo: function() {
@@ -20,7 +43,7 @@ var StopsContainer = React.createClass({
         .then(function(res){return res.json()}.bind(this))
           .then(function(data){
             var agencyList = this.sortData(data);
-            this.setState({stops: data, agencies: agencyList});
+            this.setState({stops: data, allStops: data, agencies: agencyList});
           }.bind(this));
     }.bind(this))
   },
@@ -87,7 +110,7 @@ var StopsContainer = React.createClass({
     console.log("hello")
     console.log(window.lat, window.lon)
     var latlon = window.lat + "," + window.lon
-    if (this.state.stops !== null) {
+    if (this.state.stops !== null && this.state.stops.length != 0) {
       var stops = Object.keys(this.state.agencies).map(function(stop){
       var stopLink = "http://www.google.com/maps/dir/" + latlon + "/" + stop + "/data=!4m2!4m1!3e2"
         return (
@@ -100,6 +123,14 @@ var StopsContainer = React.createClass({
           </div>
         );
       }.bind(this))
+    } else if (this.state.stops != null && this.state.stops.length == 0) {
+      return (
+        <div className="stop-container col-sm-12 col-md-12 col-lg-12">
+          <div className="header-block col-sm-12 col-md-12 col-lg-12">
+            <div className="stop-name col-sm-8 col-md-8 col-lg-8">No Closeby Stops</div>
+          </div>
+        </div>
+        );
     } else {
       return <div><Loading/></div>
     }
